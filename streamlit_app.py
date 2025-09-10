628db56f6722413e00ea27d58abd18e0c23ec710
yunqiji <jiyunqi@gmail.com>
2025-09-09 23:15:18 -0600

    Update streamlit_app.py
    add panda package

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 #from snowflake.snowpark.context import get_active_session  removed
 from snowflake.snowpark.functions import col
 import requests
+import panda
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
