7c9cc028d6eaf5ff943989c916fb46da6fa14971
yunqiji <jiyunqi@gmail.com>
2025-09-09 21:36:44 -0600

    Update streamlit_app.py
    another change made for moving from SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,8 @@ st.write('The name of your smoothie will be',name_on_order)
 
 
 #If the line session = get_active_session() appears in your code two times, delete one of the lines. 
-session = get_active_session()
+cnx = st.connection("snowflake")  ##added for moving from SiS to SniS
+session = cnx.session() # old version is get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
